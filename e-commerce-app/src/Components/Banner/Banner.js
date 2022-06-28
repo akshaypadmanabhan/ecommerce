a51b7f0c950b5e18f10b7030a38f87491f40e86a
Akshay Padmanabhan <akshayplearning@gmail.com>
2022-06-29 01:36:48 +0530

    today-work-done

--- e-commerce-app/src/Components/Banner/Banner.js
+++ e-commerce-app/src/Components/Banner/Banner.js
@@ -1,33 +1,11 @@
 import React from 'react';
 
 import './Banner.css';
-import Arrow from '../../Assets-2/Arrow'
+
 function Banner() {
   return (
-    <div className="bannerParentDiv">
-      <div className="bannerChildDiv">
-        <div className="menuBar">
-          <div className="categoryMenu">
-            <span>ALL CATEGORIES</span>
-            <Arrow></Arrow> 
-          </div>
-          <div className="otherQuickOptions">
-            <span>Cars</span>
-            <span>Motorcy...</span>
-            <span>Mobile Ph...</span>
-            <span>For Sale:Houses & Apart...</span>
-            <span>Scoot...</span>
-            <span>Commercial & Other Ve...</span>
-            <span>For Rent: House & Apart...</span>
-          </div>
-        </div>
-        <div className="banner">
-          <img
-            src="../../../Images/banner copy.png"
-            alt=""
-          />
-        </div>
-      </div>
+    <div className="banner">
+      <img className="banner-image"alt="banner" src='https://previews.123rf.com/images/arrow/arrow1508/arrow150800011/43834601-online-shopping-e-commerce-flat-design-concept-banner-background.jpg'/>
       
     </div>
   );
